d5e8f823308123836599e9cd4c0753db5107da14
BlackKaiser <88788532+blackkaiserxjc@users.noreply.github.com>
2022-01-05 01:00:53 +0800

    add: crc修改单元测试

--- askr/utility/crc.h
+++ askr/utility/crc.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <climits>
+
 #include <askr/utility/detail/crc_table.h>
 
 namespace askr {
@@ -32,25 +34,67 @@ public:
     }
 
     /**
+     * 整数更新
+     * @param value 数据
+     */
+    template <typename T, typename std::enable_if_t<std::is_integral<T>::value, int> = 0>
+    constexpr void update(T value) noexcept
+    {
+        [&]<std::size_t... N>(std::index_sequence<N...>)
+        {
+            ((rem_ = detail::crc_table<Bits>::update(rem_, (value >> N * CHAR_BIT) & 0xff)), ...);
+        }
+        (std::make_index_sequence<sizeof(T)>{});
+    }
+
+    /**
+     * 浮点数更新
+     * @param value 数据
+     */
+    constexpr void update(float value) noexcept
+    {
+        union
+        {
+            float f;
+            uint32_t i;
+        } u{value};
+        update(static_cast<uint32_t>(u.i));
+    }
+
+    constexpr void update(double value) noexcept
+    {
+        union
+        {
+            double d;
+            uint64_t i;
+        } u{value};
+        update(static_cast<uint64_t>(u.i));
+    }
+
+    /**
+     * 字符串更新
+     * @param str 字符串
+     */
+    constexpr void update(std::string_view str) noexcept
+    {
+        for (auto c : str)
+        {
+            update(c);
+        }
+    }
+
+    /**
      * 更新
      * @param data 数据
      * @param size 数据大小
      */
-    constexpr void update(void const *data, std::size_t size)
+    void update(void const *data, std::size_t size)
     {
         auto const *start = static_cast<const std::uint8_t *>(data);
         auto const *end = start + size;
         for (auto p = start; p < end; ++p)
         {
             rem_ = detail::crc_table<Bits>::update(rem_, *p);
-        }
-    }
-
-    constexpr void update(std::string_view str)
-    {
-        for (auto c : str)
-        {
-            rem_ = detail::crc_table<Bits>::update(rem_, c);
         }
     }
 
--- askr/utility/test/crc_unittest.cpp
+++ askr/utility/test/crc_unittest.cpp
@@ -6,13 +6,8 @@
 constexpr uint32_t crc32(std::string_view str)
 {
     askr::crc<32> crc;
-    crc.update(str.data(), str.size());
+    crc.update(str);
     return crc.checksum();
-    /*
-    uint32_t crc = 0xffffffff;
-    for (auto c : str)
-        crc = askr::detail::crc_table<32>::update(crc, c);
-    return crc ^ 0xffffffff;*/
 }
 
 BOOST_AUTO_TEST_SUITE(CRC)
@@ -22,6 +17,12 @@ BOOST_AUTO_TEST_CASE(CompileTimeTest)
 {
     // BOOST_CHECK_EQUAL((crc32("123456789")), 0xCBF43926);
     static_assert(crc32("123456789") == 0xCBF43926);
+
+    askr::crc<32> crc;
+    crc.update(12345678);
+    crc.update(3.14f);
+    crc.update(3.14);
+    crc.checksum();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
